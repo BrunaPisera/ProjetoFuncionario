9f30f48399e71ec0bedba1d683cdeaf986637ed9
unknown <pissara2@gmail.com>
2020-06-14 21:17:39 -0300

    Subindo o codigo para o github

---
+++ src/Empregado.java
@@ -0,0 +1,30 @@
+public class Empregado {
+    private String name;
+    private String sobrenome;
+    private double salary;
+
+    public Empregado(String name, String sobrenome, double salary) {
+        this.name =  name;
+        this.sobrenome = sobrenome;
+        if(salary < 0){
+            this.salary = 0;
+        }else{
+           this.salary =  salary;
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getSobrenome(){
+        return sobrenome;
+    }
+    public double getSalary (){
+        return salary;
+    }
+
+    public void setRaseSalary (){
+        //salary = this.salary;
+        this.salary = this.salary*(1.1);
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args){
+        Empregado empregado1 = new Empregado("Bruna", "Pissara", -10);
+        Empregado empregado2 = new Empregado("Matheus", "Pissara", 200);
+
+        //System.out.println(empregado1.getName());
+        //System.out.println(empregado1.getSobrenome());
+        System.out.println(empregado1.getSalary());
+
+        //System.out.println(empregado2.getName());
+        //System.out.println(empregado2.getSobrenome());
+        System.out.println(empregado2.getSalary());
+
+        empregado1.setRaseSalary();
+        empregado2.setRaseSalary();
+
+        System.out.println(empregado1.getSalary());
+        System.out.println(empregado2.getSalary());
+    }
+
+}
